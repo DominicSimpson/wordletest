6fe36d5e13bd3c6883fbcc04724ee3cbdc82b8a3
Dominic Simpson <db_simpson@yahoo.co.uk>
2022-11-07 17:09:26 +0000

    Added key appearing on screen functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,41 @@
-console.log("Hello world!");
 
-// const wordleWord = ['R','A','I','S','E'];
+const guessedWords = [[]];
 
 document.addEventListener("DOMContentLoaded", () => {
 
 const keys = document.querySelectorAll(".keyboard-row button");
 
+let availableSpace = 1;
+
 for (let i = 0; i < keys.length; i++) {
 
     keys[i].onclick = ( { target } ) => {
 
-        const key = target.getAttribute("data-key");
+        const letter = target.getAttribute("data-key");
 
-        console.log(key);
+        console.log(letter);
 
+        updateGuessedWords(letter);
+
+    }
+}
+
+function getCurrentWordArr() {
+    const numberOfGuessedWords = guessedWords.length;
+    return guessedWords[numberOfGuessedWords - 1];
+}
+
+function updateGuessedWords(letter) {
+    const currentWordArr = getCurrentWordArr(); 
+    
+    if (currentWordArr && currentWordArr.length < 5) {
+        currentWordArr.push(letter);
+
+        const availableSpaceElement  = document.getElementById(String(1));
+
+        availableSpace= availableSpace + 1;
+
+        availableSpaceElement.textContent = letter;
     }
 }
 
