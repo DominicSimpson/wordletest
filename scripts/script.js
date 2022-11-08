6da8bd09fc97b8f176eb61f5d4be28252ba93a8f
Dominic Simpson <db_simpson@yahoo.co.uk>
2022-11-08 15:40:39 +0000

    Added heading

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,78 @@
+document.addEventListener("DOMContentLoaded", () => {
 
 let guessedWords = [[]];
 
-document.addEventListener("DOMContentLoaded", () => {
 
-let keys = document.querySelectorAll(".keyboard-row button");
+let keys = document.querySelectorAll('.keyboard-row button');
 
 let availableSpace = 1;
 
-let word = 'raise';
+let word = 'raise'; // Correct word
+
+
 let guessedWordCount = 0;
 
+
+function getTileColor (letter, index) {
+    const isCorrectLetter = word.includes(letter);
+
+    if (!isCorrectLetter) {
+        return "rgb(58,58,60)";
+
+    }
+
+    const letterInPosition = word.charAt(index); // Returns the correct letter in the word
+    console.log(letterInPosition);
+    const isCorrectPosition = letter === letterInPosition;    
+
+    if (isCorrectPosition) {
+        return "rgb(83,141,78)";
+
+    }
+        return "rgb(181,159,59)";
+
+}
+
+
 function handleSubmitWord() {
-    const currentWordArr = getCurrentWordArr();
 
-    const currentWord = currentWordArr.join(''); // joins word into a string
+    let currentWordArr = getCurrentWordArr();
 
-    const firstLetterId = guessedWordCount * 5 + 1;
-            
-    // const interval = 300;
+    if (currentWordArr.length !== 5) {
+        window.alert("Answer must be five letters");
+    }
 
+    let currentWord = currentWordArr.join(''); // joins word into a string
     
-    currentWordArr.forEach((letter, index) => {
-        setTimeout(() => {
-            const letterSquareColour = "rgb(255,165,0)";
+    const firstLetterId = guessedWordCount * 5 + 1;
 
+    const interval = 200;
+
+       currentWordArr.forEach((letter, index) => {
+        setTimeout(() => {
+            const tileColor = getTileColor(letter, index);
 
             const letterId = firstLetterId + index;
-            const letterElement = document.getElementById(letterId);
-            letterElement.classList.add('animate__flipInY');
-            letterElement.style = `background-color:${letterSquareColour};border-color:${letterSquareColour}`;
+            
+            const letterEl = document.getElementById(letterId);
+            
+            letterEl.classList.add('animate__flipInY');
+            
+            letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
 
-        }, 200);
-    })
+        }, interval * index);
+    });
 
+    guessedWordCount += 1;
+
+    if (currentWord === word) {
+        window.alert("Correct guess!");
+    }
+
+
+    guessedWords.push([]);
+
+            
 }
 
 
@@ -40,11 +80,11 @@ for (let i = 0; i < keys.length; i++) {
 
     keys[i].onclick = ( { target } ) => {
 
-        const letter = target.getAttribute("data-key");
+        const letter = target.getAttribute('data-key');
 
         if  (letter === 'enter') {
-            handleSubmitWord()
-            return;
+            handleSubmitWord();
+            return; 
         }
 
         console.log(letter);
@@ -67,15 +107,16 @@ function updateGuessedWords(letter) {
     if (currentWordArr && currentWordArr.length < 5) {
         currentWordArr.push(letter);
 
-        const availableSpaceElement  = document.getElementById(availableSpace);
+        const availableSpaceEl  = document.getElementById(availableSpace);
 
         availableSpace = availableSpace + 1;
 
-        availableSpaceElement.textContent = letter;     
+        availableSpaceEl.textContent = letter;     
         
     }
 
 
     }
+
 
 });
