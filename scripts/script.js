a1959143e0ba928131c188c0dda5d1af82d50e35
Dominic Simpson <52511353+DominicSimpson@users.noreply.github.com>
2022-12-05 22:56:55 +0000

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,160 @@
+document.addEventListener("DOMContentLoaded", () => { // loads the file
+
+let guessedWords = [[]]; // Array containing array of guessed word
+
+let keys = document.querySelectorAll('.keyboard-row button'); // stores each key (A-Z) in one variable
+
+let availableSpace = 1; // initialised number of letters available in each attempt at a guess
+
+let correctWord = 'raise'; // Correct word
+
+let guessedWordCount = 0; // increments to one at end of this test
+
+let finalWordArray = []; // contains values of final submitted word
+
+
+function checkLetter (letter, index) {
+    const isCorrectLetter = correctWord.includes(letter); // checks if correct word features inputted letter
+                                                          // includes method returns boolean true or false
+
+    if (!isCorrectLetter) { // if no matches are found
+        console.log('finalWordArray', finalWordArray.length);
+
+        for(let val=1 ; val <= finalWordArray.length; val++){
+        console.log('value',val);
+        
+        setTimeout(()=>{
+        document.getElementById(val).style.color = "white";
+        }, 1000)
+        
+    }
+        return "rgb(58,58,60)";
+    }
+
+    const letterInPosition = correctWord.charAt(index); // Returns the respective letter of each numeric point 
+                                                        // in the correct word, 
+                                                        // e.g., in correct word, r for 1, a for 2, etc.
+    console.log(letterInPosition);
+    const ifLetterFeaturesInCorrectWord = letter === letterInPosition; // tests if inputted letter features in correct word, 
+                                                                       // and is in correct position    
+
+    if (ifLetterFeaturesInCorrectWord) {
+        return "rgb(83,141,78)"; // if inputted letter is in same position as corresponding letter in correct word
+
+    }
+        return "rgb(181,159,59)"; // if inputted letter is in correct word, but not in corresponding position
+
+}
+
+function handleSubmittedWord() {
+
+    let currentWordArr = getCurrentWordArray();
+
+    document.getElementById("gameresponse").innerHTML = '';
+   
+
+    let currentWord = currentWordArr.join(''); // join method is used turn five letters in array into a string
+    
+    const firstLetterId = guessedWordCount * 5 + 1; // the first letter ID in an attempt at a word
+                                                    // guessedWordCount * 5 + 1 would be the first letter ID in each row 
+                                                    // if there were multiple rows
+
+    const interval = 200; // setTimeout interval time
+
+       currentWordArr.forEach((letter, index) => {
+        setTimeout(() => { // asychronous JS for applying colour effects to letters after user presses Enter
+            const letterSquare = checkLetter(letter, index);
+
+            let letterId = firstLetterId + index; // gets the ID of each specific letter in an attempted word
+                                                  // (zero-index based)
+            console.log(letterId);
+            
+            const letterElement = document.getElementById(letterId); // stores letterID of each specific letter in variable
+                        
+            letterElement.style = `background-color:${letterSquare};border-color:${letterSquare}`;
+
+        }, interval * index); // interval asychronous JS occurs for each letter once user has pressed Enter
+    });
+
+    guessedWordCount += 1; // increments the amount of guessed words by one
+
+    if (currentWordArr.length !== 5) { // Alerts user if they press Enter without inputting in five letters
+        console.log("User has not entered in five letters", currentWordArr.length);
+       // body.className = "success"; // Unleashes CSS /// console.log('numberOfWORDS', numberOfGuessedWords);
+
+        document.getElementById("gameresponse").innerHTML = "Answer must be five letters";
+   
+       
+    }
+  
+    if (currentWord === correctWord) {
+        document.getElementById("gameresponse").innerHTML = "Correct word! Congratulations!";
+    }
+    guessedWords.push([]); // pushes attempt at guessing correct word into array
+            
+}
+
+function handleDeletedLetter() {
+    const currentWordArr = getCurrentWordArray(); // same variable as declared in handleSubmitWord function
+    currentWordArr.pop(); // pop method is used to delete last letter one by one
+
+    guessedWords[guessedWords.length - 1] = currentWordArr; // takes the currentWordArr and assigns it to guessWords
+                                                            // variable without last letter
+    const lastLetter = document.getElementById(String(availableSpace - 1)); // String constructor is used to create a new string object
+    // lastLetter is always the last letter in the five-letter sequence
+    lastLetter.textContent = "";
+    availableSpace = availableSpace - 1;
+}
+
+
+for (let i = 0; i < keys.length; i++) { // gets the selected letter from the keypad
+
+    keys[i].onclick = ( { target } ) => {
+
+        const letter = target.getAttribute('data-key'); // gets the individual letter
+
+        if  (letter === 'enter') { // if the user clicks enter
+            handleSubmittedWord();
+            return; 
+        }
+
+        if (letter === 'delete') { // if the user clicks delete
+            handleDeletedLetter(); 
+            return;
+        }
+
+        console.log(letter);
+
+        updateGuessedWords(letter);
+
+    }
+}
+
+function getCurrentWordArray() { 
+    const numberOfGuessedWords = guessedWords.length; // stores number of guessed words in variable
+   
+    return guessedWords[numberOfGuessedWords - 1]; // subtracts -1 from the number of guessed words after user inputs guessed word
+}
+
+function updateGuessedWords(letter) { 
+    let currentWordArr = getCurrentWordArray(); // takes the function above and updates the current word array
+
+    console.log(currentWordArr);
+    
+    if (currentWordArr && currentWordArr.length < 5) { // Updates each currentWordArray, and pushes inputted letter into array
+        currentWordArr.push(letter);                   // if less than five 
+
+        finalWordArray = currentWordArr;                // this then becomes the finalWordArray, which has a value for each letter
+        console.log('finalWordArray',finalWordArray);
+
+        const emptyLetterSquare  = document.getElementById(availableSpace); // emptyLetterSquare corresponds to each available 
+                                                                                // lettersquare / box
+        availableSpace = availableSpace + 1; // updates amount of available space
+
+        emptyLetterSquare.textContent = letter; // letter is passed into each available lettersquare / box 
+        
+    }
+
+    }
+
+});
